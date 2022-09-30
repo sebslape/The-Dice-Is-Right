55a0a721738ee40d5b14a46ec3fd42d509e39891
SebastianSlape <sebslape@yahoo.com>
2022-09-30 12:09:35 +0930

    Update

--- main.js
+++ main.js
@@ -22,18 +22,39 @@ function sanitiseNumPlayers(number) {
     return number; // Return the sanitised number
 }
 
+function finishFade() {
+    gameContainer.style.opacity = "1"; // Update game container
+}
+
+function finishSetup() {
+    startContainer.style.display = "none"; // Hide the start container
+    
+    gameContainer.style.display = "block"; // Show the game container
+    setTimeout(() => { finishFade() }, 1); // Fixes glitch with game container not fading in
+
+    canRoll = true; // Allow the player to roll the dice
+}
+
 function onStart() {
 
     turn = 0; // Reset the turn to zero
     players = sanitiseNumPlayers(numPlayers.value); // Sanitise the number of players VIA clamping
     playerScores = []; // Reset player scores
 
-    for (let i = 0; i < 5; i++) { // Reset the text of the scoreboard
-        playerScores.push(0); // Fill the player scores array with zeros
+    // Loop through all of the possible players
+    for (let i = 0; i < 5; i++) {
+
+        // Fill the player scores array with zeros
+        playerScores.push(0);
+
+        //Reset the text of the scoreboard
         document.getElementById("player" + (i + 1)).textContent = "";
         document.getElementById("player" + (i + 1) + "History").textContent = "";
     }
-    for (let i = 0; i < players; i++) { // Set the text of the scoreboard for each of the players
+    // Loop through all of the actual players
+    for (let i = 0; i < players; i++) {
+
+        // Set the text of the scoreboard for each of the players
         document.getElementById("player" + (i + 1)).textContent = "Player " + (i + 1) + " - 0";
         document.getElementById("player" + (i + 1) + "History").textContent = "...";
     }
@@ -46,37 +67,65 @@ function onStart() {
         []
     ]; 
 
-    startContainer.style.display = "none"; // Hide the start container
-    gameContainer.style.display = "block"; // Show the game container
-    canRoll = true; // Allow the player to roll the dice
+    // Update title to fit screen
+    title.style.fontSize = "4em";
+    title.style.padding = "20px 0px";
+
+    // Update starting container
+    startContainer.style.opacity = "0";
+
+    setTimeout(() => { finishSetup() }, 600); // Wait until title transition is finished
 }
 
 startBtn.onclick = onStart;
 
 function checkForSpecialRolls(playerScores, playerHistory, roll) {
     if (roll == 1) { // If the roll is one, set the roller's score to zero
+
+        // Display the roll
         statusBar.textContent = "Player " + ((turn % players) + 1).toString() + " rolled a 1 resetting their score!";
-        playerScores[turn % players] = 0;
-        return 0;
+
+        playerScores[turn % players] = 0; // Set the roller's score to zero
+        return 0; // Add zero to the players total
     }
+
+    // If the player has rolled twice or more, continue (Prevents array access errors)
     if (playerHistory.length >= 2) {
+
+        // If the player has rolled two of the same number in a row, continue
         if (roll == playerHistory[playerHistory.length - 2]) {
+
+            // If the player has rolled thrice or more, continue (Prevents array access errors)
             if (playerHistory.length >= 3) {
-                if (roll == playerHistory[playerHistory.length - 3]) {
+
+                // If the player has rolled three of the same number in a row, continue
+                if (roll == playerHistory[playerHistory.length - 3]) { 
+
+                    // Display the roll
                     statusBar.textContent = "Player " + ((turn % players) + 1).toString() + " rolled three " + roll + "'s giving them " + (roll * 3) + "!";
+
+                    // Return the roll
                     return roll * 3;
                 }
             }
+
+            // Display the roll
             statusBar.textContent = "Player " + ((turn % players) + 1).toString() + " rolled two " + roll + "'s giving them " + (roll * 2) + "!";
+
+            // Return the roll
             return roll * 2;
         }
     }
+
+    // Display the roll
     statusBar.textContent = "Player " + ((turn % players) + 1).toString() + " rolled a " + roll + "!";
+
+    // Return the roll
     return roll;
 }
 
 function changeDice() {
-    let roll = Math.round(Math.random() * 5) + 1; // Choose a random number between 1 and 6
+    let roll = Math.round(Math.random() * 5) + 1; // INT - Choose a random number between 1 and 6
     dice.src = "Dice/Dice-" + roll + ".svg"; // Update dice image
 }
 
@@ -91,13 +140,16 @@ function updateDiceRoll() {
     roll = checkForSpecialRolls(playerScores, rollHistory[turnPlayer], roll); // Calculate the dice rolls score by checking for doubles, triples, and rolls of 1's
     playerScores[turnPlayer] += roll; // Add the roll to the players score
 
+    // Create the string that will replace the players scoreboard entry
     scoreText = "Player " + (turnPlayer + 1) + " - " + playerScores[turnPlayer];
     historyText = rollHistory[turnPlayer].join(", ");
 
+    // Update the score and the roll history of the player who rolled
     document.getElementById("player" + (turnPlayer + 1)).textContent = scoreText;
     document.getElementById("player" + (turnPlayer + 1) + "History").textContent = historyText;
 
-    if (playerScores[turnPlayer] >= 30) { // End the game if the player has reached more than 30 points
+    // End the game if the player has reached more than 30 points
+    if (playerScores[turnPlayer] >= 30) {
         statusBar.textContent = "Player " + ((turn % players) + 1).toString() + " reached 30 first and won!";
         return;
     }
@@ -114,9 +166,9 @@ function onDiceRoll() {
     canRoll = false; // Make it so the player can't roll during the animation
 
     for (let i = 0; i < 9; i++) {
-        setTimeout(() => { changeDice() }, i*100);
+        setTimeout(() => { changeDice() }, i*100); // Change the dice image nine times with 100 milliseconds in between
     }
-    setTimeout(() => { updateDiceRoll() }, 1000);
+    setTimeout(() => { updateDiceRoll() }, 1000); // Update the dice after 1000 milliseconds, 100 milliseconds after the animation has finished
 }
 
 dice.onclick = onDiceRoll;
